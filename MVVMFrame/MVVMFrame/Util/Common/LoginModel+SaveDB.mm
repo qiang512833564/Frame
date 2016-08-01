de6bfea9170c1dcab881fe38ef76d3ad32ef4a10
HW <hw@192.168.0.101>
2016-08-02 00:11:37 +0800

    更新

--- MVVMFrame/MVVMFrame/Util/Common/LoginModel+SaveDB.mm
+++ MVVMFrame/MVVMFrame/Util/Common/LoginModel+SaveDB.mm
@@ -10,8 +10,24 @@
 #import <iostream>
 #import <vector>
 #import <objc/runtime.h>
+#warning 总结：模板类的模板参数\
+         如：template <typename T, int N>  这里 <> 尖括号里面的参数内容，可以是 typename 表示任意类型, int , enum 等，同时，这里的参数尤其得注意，这个就是下面模板类 String 的声明格式，也就是说，如果需要用到 String模板的时候，得 String<T,N> 格式的样子使用，另外，需要注意的是：这里的 T,N 类型，如果在 String 的内部用到了，也得注意参数类型一样，否则会出现编译不通过\
+            class String:std::string {\
+                String(T name){} \
+            };
 
 @implementation LoginModel (SaveDB)
+/*
+    C++中模板的声明和定义的位置一定要小心处理, 否则在链接时会出现"无法解析某某函数或类"的错误。
+ 　　函数模板的一般定义形式：
+ 　　template < 类型形式参数表 > 返回类型 FunctionName( 形式参数表 )
+ 　　{
+ 　　// 函数定义体
+ 　　}
+ 　　说明：
+ 　　⒈ < 类型形式参数表 > 可以包含基本数据类型，也可以包含类类型。若是类类型，则须加前缀 class 。
+ 　　⒉这样的函数模板定义不是一个实实在在的函数，编译系统不为其产生任何执行代码。该定义只是对函数的描述，表示它每次能单独处理在类型形式参数表中说明的数据类型。
+ */
 ///在 STL9中对vector 的习惯用法完全不同。我们不是定义一个已知大小的 vector，而是定义一个空 vector \
 vector< string > text;
 
@@ -60,20 +76,23 @@ struct Teacher {
 };
 
 
-template <typename T>
+template <typename T, int N>
 class Student {
 public:
     Student() {
         
     }
     Student(T name){
-        std::cout <<name << std::endl;
+        std::cout <<name <<std::endl << N << std::endl;
     }
 };
 
-//template<> std::vector<std::string>::~vector() {
-//    
-//}
+template <typename T>
+inline T const& max(T const &a, T const &b)
+{
+    return a<b?b:a;
+}
+
 + (void)load {
     std::vector<std::string> array = [self getIvarArray];
     for (auto &i :array) {
@@ -101,9 +120,15 @@ public:
        temp.swap(array);
     }
     
-    Student<Teacher> student;
+    Student<int,100> student(10);
     int a = 10, b =20;
     __swap(a,b);
+    
+    
+    ::max("a","b");         //正确
+    //::max("a","ab");        //错误。因为当成引用时，匹配过程包括数组的维数。把模板参数改成指针就可以通过了
+    //std::string s;
+    //::max("a",s);           //srting和这里的字符数组没办法匹配
 }
 
 @end
