80f7961ede1fca1c14f2e87c51c4e2cc9ee8db77
HW <hw@zhangweideMacBook-Pro.local>
2016-08-02 17:30:03 +0800

    代码更新

---
+++ MVVMFrame/MVVMFrame/Base/BaseModel/RootObject.h
@@ -0,0 +1,13 @@
+//
+//  RootObject.h
+//  MVVMFrame
+//
+//  Created by HW on 16/8/2.
+//  Copyright © 2016年 lizhongqiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RootObject : NSObject
+
+@end
---
+++ MVVMFrame/MVVMFrame/Base/BaseModel/RootObject.mm
@@ -0,0 +1,13 @@
+//
+//  RootObject.m
+//  MVVMFrame
+//
+//  Created by HW on 16/8/2.
+//  Copyright © 2016年 lizhongqiang. All rights reserved.
+//
+
+#import "RootObject.h"
+
+@implementation RootObject
+
+@end
